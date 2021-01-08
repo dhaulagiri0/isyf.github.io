b62746ce06f86386fe539e3dd261a38f3299def8
Dhaulagiri0 <yiqinw4@gmail.com>
2021-01-08 21:29:15 +0800

    updated popsters

---
+++ js/posters.js
@@ -0,0 +1,266 @@
+posterData = [
+    {
+        "no": "No. 1", 
+        "title" : "A machine learning Based Approach for prediction of rTMS treatment response in  patients with drug-resistant depression using magnetoencephalographic data", 
+        "school" : "Taipei Municipal Jian Guo High School", 
+        "student":["Chia-Wei Chang"], 
+        "image":'images/posters/1', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 2", 
+        "title" : "An indirect experimental method on inactivation of viruses with viral envelope", 
+        "school" : "Chung Hwa Middle School, B.S.B. Maktab Sains Paduka Seri Begawan Sultan ", 
+        "student":["Kiefer Lim Yi Heng", "Farhanah binti Sufian"], 
+        "image":'images/posters/2', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 3", 
+        "title" : "An Investigation to compare the Vitamin C content of 3 types of berries (Blueberries, Blackberries, Raspeberries)", 
+        "school" : "St Joseph Institution", 
+        "student":["Aeden Chng Yu Khye"], 
+        "image":'images/posters/3', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 4", 
+        "title" : "Business model for private companies to invest in smart grid electricity system and the utilization of tokenization  (ICO: Initial Coin Offering) as a way to raise fund", 
+        "school" : "Mahidol Wittayanusorn", 
+        "student":["Pitchaya Paramavej"], 
+        "image":'images/posters/4', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 5", 
+        "title" : "Connect Singapore", 
+        "school" : "Hwa Chong Institution", 
+        "student":["Huang Qirui"], 
+        "image":'images/posters/5', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 6", 
+        "title" : "Corn Starch Based Bioplastic", 
+        "school" : "SMA Lazuardi GCS", 
+        "student":["Fathur Rahmat Syahputra"], 
+        "image":'images/posters/6', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 7", 
+        "title" : "Design of Underground Structure to Mitigate Effects of Soil Liquefication", 
+        "school" : "National Junior College", 
+        "student":["Mak Zhan Rui Jovan"], 
+        "image":'images/posters/7', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 8", 
+        "title" : "Effectiveness of Bovine Skin Gelatin-chitosan Coating on Limiting Microbial Activity", 
+        "school" : "River Valley High School", 
+        "student":["Chan Kai Sing Natalie"], 
+        "image":'images/posters/8', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 9", 
+        "title" : "Evaluation of Microgreen Growth Under Defined Mediums and Culture Conditions", 
+        "school" : "National Junior College", 
+        "student":["Wong Sam Tou", "Chua Zi Xin Rachel"], 
+        "image":'images/posters/9', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 10", 
+        "title" : "Fabrication of Microbial Fuels Cells Powered by Organic Waste and Wastewater to Generate Bioelectricity", 
+        "school" : "Hwa Chong Institution", 
+        "student":["Ng Yan Bin Lucas", "Lucas Koh Eu Jen"], 
+        "image":'images/posters/10', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 11", 
+        "title" : "Factors Affecting Rate of Transmissibility of SARS-CoV-2 Virus by Surface Materials", 
+        "school" : "Victoria Junior College", 
+        "student":["Chng Guang Sen Elijah", "S. Prithika", "Tan Chun Liang"], 
+        "image":'images/posters/11', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 12", 
+        "title" : "Fruits VS Fruit Juices", 
+        "school" : "CHIJ ST. THERESA'S CONVENT", 
+        "student":["Bawa Amarnath Sania", "Melody Goh Tze Ning", "Ma Qianjie"], 
+        "image":'images/posters/12', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 13", 
+        "title" : "Gait Monitoring and Analysis for Parkinson Disease Patients", 
+        "school" : "NUS High School of Mathematics and Science", 
+        "student":["Ye Chen Rui", "Prannaya Gupta", "Nallapuraju Ananya"], 
+        "image":'images/posters/13', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 14", 
+        "title" : "GTC Treatement", 
+        "school" : "SMJK Chung Ling", 
+        "student":["Tang Yi Ze", "Lim Eugene"], 
+        "image":'images/posters/14', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 15", 
+        "title" : "Handling Plastic Waste in Indonesia", 
+        "school" : "SMA Lazuardi GCS", 
+        "student":["Alisha Yahya"], 
+        "image":'images/posters/15', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 16", 
+        "title" : "Effects of Magnetic Fields on Plant Growth", 
+        "school" : "Nanyang girls high school ", 
+        "student":["Abigail Chin Yanqin"], 
+        "image":'images/posters/16', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 17", 
+        "title" : "Impacts of Aerosol-Radiation-Cloud Feedbacks on the Meteorological factors during the High PM2.5 Concentration Episodes in South Korea", 
+        "school" : "Korea Science Academy of KAIST", 
+        "student":["Daechul Kim", "Seungwoo Kim"], 
+        "image":'images/posters/17', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 18", 
+        "title" : "In silico analysis of proteolytic resistance of novel AMPs dervied from the AMP bicarinalin sequence", 
+        "school" : "Philippine Science High School - Main Campus", 
+        "student":["Clarence Lei B. Bautista", "Chen Adrien Z. Thor"], 
+        "image":'images/posters/18', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 19", 
+        "title" : "Investigating the effect of different types of vinegar as adjuvants on the toxicity of Radix Bupleuri (RB)", 
+        "school" : "River Valley High School", 
+        "student":["Zhang Yu"], 
+        "image":'images/posters/19', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 20", 
+        "title" : "Investigating the effects of adsorbents made from fresh and dried mealworms and prawns in adsorbing lead (II) ions and methyl orange.", 
+        "school" : "Raffles Institution", 
+        "student":["He Jingyang James", "Ian Ng Khai Ven", "Javier Ng Yi Sheng"], 
+        "image":'images/posters/20', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 21", 
+        "title" : "Maestro Card 2.0", 
+        "school" : "Sekolah Sultan Alam Shah, Putrajaya ", 
+        "student":["Syaf Aiman Danial bin Syafizal Anuar", "Muhammad Danish bin Azrai Izet "], 
+        "image":'images/posters/21', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 22", 
+        "title" : "Maximizing the Efficiency of Solar Panel", 
+        "school" : "Temasek Junior College", 
+        "student":["Keerthana Kailasam", "Chow Jeng Han", "Angus Yim"], 
+        "image":'images/posters/22', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 23", 
+        "title" : "School of Science and Technology, Singapore", 
+        "school" : "Hwa Chong Institution", 
+        "student":["Ong Zhi Xuan", "Tay Shao Yang Joel"], 
+        "image":'images/posters/23', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 24", 
+        "title" : "Paper aeroplanes", 
+        "school" : "Nanyang Girls’ High School", 
+        "student":["Esther Tay Shee Wei"], 
+        "image":'images/posters/24', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 25", 
+        "title" : "Reccurence relations", 
+        "school" : "Xinmin Secondary School", 
+        "student":["Mu Junrong"], 
+        "image":'images/posters/25', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 26", 
+        "title" : "Research on Electroless Copper Plating", 
+        "school" : "Ichinomiya High School", 
+        "student":["Mizuno Rio", "Hirao Momona"], 
+        "image":'images/posters/26', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 27", 
+        "title" : "See Atoms in 3D Space", 
+        "school" : "CHIJ St. Nicholas Girls’ School (Secondary)", 
+        "student":["Chin Yan Zi", "Yong Xue Yuan", "Phoon Zi Ling"], 
+        "image":'images/posters/27', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 28", 
+        "title" : "Smart Trash Bin as Chopper and Processor Household Organic Waste into Liquid Organic Fertilizer Based on Arduino and EM4", 
+        "school" : "SMA Negeri 4 Denpasar", 
+        "student":["I Putu Evan Priya Saguna", "Putu Eka Parmartha Dharma"], 
+        "image":'images/posters/28', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 29", 
+        "title" : "Strangle Locust Plague at Birth", 
+        "school" : "The second high school attached to the Beijing Normal University", 
+        "student":["Qingyu Liu", "Moqing Liang"], 
+        "image":'images/posters/29', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 30", 
+        "title" : "Synthesis and Characterisation of the TI02 Photocatalyst for Treatment of Polluted Wastewater in Van Phuc Silk Village", 
+        "school" : "High School for Gifted Students, Hanoi National University of Education THPT chuyên Hà Nội - Amsterdam", 
+        "student":["Thai Hoang Son", "Đỗ Quang Minh"], 
+        "image":'images/posters/30', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 31", 
+        "title" : "Analysis of the Skin Effect and Electronic Corona in High Voltage Long Distance Electricity Transmission", 
+        "school" : "The High School Affiliated to Xian Jiaotong University", 
+        "student":["Qiyuan Wei", "Yuhan Ren"], 
+        "image":'images/posters/31', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 32", 
+        "title" : "The Impact of Bushfires on Cyanobacteria Growth", 
+        "school" : "Presbyterian Ladies College Sydney", 
+        "student":["Sarah Ge", "Lilian Stock"], 
+        "image":'images/posters/32', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    },
+    {
+        "no": "No. 33", 
+        "title" : "To Investigate the Effect of Different Types of Yeast Strains on Fermentation of Cotton and Sawdust for Production of Bioethanol", 
+        "school" : "Nan Chiau High School", 
+        "student":["Tan Keng How", "Tan Shu En, Summer", "Dominic Aw Rui De"], 
+        "image":'images/posters/33', 
+        "vid":'https://www.youtube.com/embed/dJOT4-SwmUY'
+    }
+];
