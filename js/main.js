768bb5b54d0c9ef6efe10fd1d2c5475da2d58e77
Dhaulagiri0 <yiqinw4@gmail.com>
2021-01-19 22:00:45 +0800

    top posters display

--- js/main.js
+++ js/main.js
@@ -87,7 +87,7 @@ function formatDate(date, format, utc) {
     format = format.replace(/(^|[^\\])HH+/g, "$1" + ii(H));
     format = format.replace(/(^|[^\\])H/g, "$1" + H);
 
-    var h = H > 12 ? H - 12 : H == 0 ? 12 : H;
+    var h = H == 0 ? 12 : H;
     format = format.replace(/(^|[^\\])hh+/g, "$1" + ii(h));
     format = format.replace(/(^|[^\\])h/g, "$1" + h);
 
